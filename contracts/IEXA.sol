19e5c07f890a0f31bcbda8e432c42fcb47a1a206
NicoBarragan <nbarragan01@gmail.com>
2022-06-22 21:42:52 -0300

    feat(IEXA.sol): created and compiled successfully EXA token interface

---
+++ contracts/IEXA.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IEXA is IERC20 {
+    function mint(address _to, uint256 _ethAmount) external;
+
+    function burn(address _from, uint256 _ethAmount) external;
+
+    function addEthBalance() external payable;
+}
