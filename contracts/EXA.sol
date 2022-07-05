97388ae955f026a5e3cc1f074f5df2a1e1ca642c
NicoBarragan <nbarragan01@gmail.com>
2022-07-05 01:09:45 -0300

    feat(EXA.sol): added console.log statements for testing, removed 'payable' from 'addEthBalance' function and added '_initialSupply' in order to avoid a 0 in the calculations of 'ethPerUnit'

--- contracts/EXA.sol
+++ contracts/EXA.sol
@@ -3,33 +3,33 @@
 pragma solidity ^0.8.9;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IEXA} from "./IEXA.sol";
+import "hardhat/console.sol";
 
 // errors
 error Error__AmountIsZero();
 error Error_InvalidAddress(address invalidAddress);
 error Error_SenderIsNotTeam();
 
-contract EXA is ERC20, Ownable, ReentrancyGuard {
+contract EXA is IEXA, ERC20, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     // token variables and constants
     uint256 private _totalEthAmount;
     uint256 private _ethPerUnit;
-    uint256 private _totalSupply;
     string public constant NAME = "Exactly Token";
     string public constant SYMBOL = "EXA";
+    uint256 private constant _initialSupply = 1 * 10**18; // SUPPLY EXA = SUPPLY ETH
 
     // user variables
     uint256 private _exaAmount;
 
     constructor() ERC20(NAME, SYMBOL) {
         _totalEthAmount = 0;
-        _ethPerUnit = 0;
-        _totalSupply = 1 * 10**18;
+        _ethPerUnit = 0; // TODO: 1? for avoid an error when updating and dividing
     }
 
     modifier checkAmount(uint256 amount) {
@@ -49,12 +49,14 @@ contract EXA is ERC20, Ownable, ReentrancyGuard {
         }
 
         if (_ethPerUnit == 0) {
-            _ethPerUnit += (_totalSupply / _ethAmount);
+            _ethPerUnit += (_initialSupply / _ethAmount);
         }
 
+        // need _mint here for updating totalSupply before calculating ethPerUnit
         _exaAmount = _ethAmount * _ethPerUnit;
-        _totalEthAmount += _ethAmount;
         _mint(_to, _exaAmount);
+
+        _totalEthAmount += _ethAmount;
         _updateBalance();
     }
 
@@ -62,7 +64,7 @@ contract EXA is ERC20, Ownable, ReentrancyGuard {
         address _from,
         uint256 exaAmount,
         uint256 _ethAmount // is calculated on ETHPool so is cheaper insert it as parameter and not calculate again
-    ) external checkAmount(_ethAmount) nonReentrant {
+    ) external checkAmount(_ethAmount) {
         (, _totalEthAmount) = _totalEthAmount.trySub(_ethAmount);
         _burn(_from, exaAmount);
         _updateBalance();
@@ -70,21 +72,23 @@ contract EXA is ERC20, Ownable, ReentrancyGuard {
 
     function addEthBalance(uint256 _ethAmount)
         external
-        payable
         onlyOwner
         checkAmount(_ethAmount)
-        nonReentrant
     {
         _totalEthAmount += _ethAmount;
         _updateBalance();
     }
 
     function _updateBalance() internal {
-        _totalSupply = totalSupply();
-        (, _ethPerUnit) = _totalSupply.tryDiv(_totalEthAmount);
+        uint256 _totalSupply2 = super.totalSupply();
+        console.log("_totalSupply2", _totalSupply2);
+        console.log("total eth amount from exa token", _totalEthAmount);
+        (, _ethPerUnit) = _totalSupply2.tryDiv(_totalEthAmount);
+        console.log("ethPerUnit (0)", _ethPerUnit);
     }
 
     function getEthPerUnit() external view returns (uint256) {
+        console.log("ethPerUnit", _ethPerUnit);
         return _ethPerUnit;
     }
 }
