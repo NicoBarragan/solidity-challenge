822f6c2e30a6949350d3dcae65f2d86de87c8c73
NicoBarragan <nbarragan01@gmail.com>
2022-07-11 02:04:00 -0300

    feat(ETHPool.sol): integrated pool with eToken all in 1 contract

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -8,6 +8,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "hardhat/console.sol";
 
 // errors
 error Error__NotEnoughAmount(uint256 amountRequested, uint256 balance);
@@ -29,8 +30,9 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
     AggregatorV3Interface public immutable priceFeedV3Aggregator;
 
     // eToken variables and constants
-    uint256 private constant _initialMintSupply = 1 * 10**18;
-    uint256 private _totalEthAmount = 0;
+    uint256 private constant _initialMintSupply = 1 * 10**36; // big enough in order to make divisible
+    uint256 private _totalEthAmount = 0; // is necessary because balance doesn't update immediately
+    uint256 private _totalExaAmount = 0; // is necessary because balance doesn't update immediately
     uint256 private _ethPerUnit = 0;
 
     constructor(
@@ -62,10 +64,24 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
     }
 
     // method for supply ETH to the pool and mint EXA tokens to the sender
-    function supply() external payable nonReentrant {
-        _mintEToken(msg.sender, msg.value);
+    function supply() external payable checkAmount(msg.value) nonReentrant {
+        if (msg.value == 0) {
+            revert Error__AmountIsZero();
+        }
 
-        emit Supply(msg.sender, msg.value);
+        uint256 ethAmount = msg.value;
+        _totalEthAmount += ethAmount;
+
+        if (_ethPerUnit == 0) {
+            _ethPerUnit = (_initialMintSupply / ethAmount);
+        }
+
+        uint256 _exaAmount;
+        _exaAmount = ethAmount * _ethPerUnit;
+        _totalExaAmount += _exaAmount;
+        _mintEToken(msg.sender, _exaAmount);
+
+        emit Supply(msg.sender, ethAmount);
     }
 
     // method for supply DAI to the pool, convert to ETH amount and mint EXA tokens to the sender
@@ -92,8 +108,18 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
             revert Error__DivFailed(stableAmount, uint256(ethStablePrice));
         }
 
+        _totalEthAmount += ethAmount;
         stablecoin.transferFrom(msg.sender, address(this), stableAmount);
-        _mintEToken(msg.sender, ethAmount);
+        /* This is for avoid a division error when mint() is the
+         * first interaction with the contract and _ethPerUnit is equal zero */
+        if (_ethPerUnit == 0) {
+            _ethPerUnit = (_initialMintSupply / ethAmount);
+        }
+
+        uint256 _exaAmount;
+        _exaAmount = ethAmount * _ethPerUnit;
+        _totalExaAmount += _exaAmount;
+        _mintEToken(msg.sender, _exaAmount);
 
         emit Supply(msg.sender, ethAmount);
     }
@@ -115,19 +141,35 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
             );
         }
 
+        console.log("lpAmount", lpAmount);
         (bool success, uint256 ethAmount) = lpAmount.tryDiv(_ethPerUnit);
         if (!success || ethAmount == 0) {
             revert Error__DivFailed(lpAmount, _ethPerUnit);
         }
-
+        console.log("ethAmount: ", ethAmount);
+        console.log("_ethPerUnit: ", _ethPerUnit);
+        console.log("_totalEthAmount: ", _totalEthAmount);
+        (, _totalEthAmount) = _totalEthAmount.trySub(ethAmount);
+        console.log("_totalEthAmount: ", _totalEthAmount);
+        (, _totalExaAmount) = _totalExaAmount.trySub(lpAmount);
         payable(msg.sender).transfer(ethAmount);
-        _burnEtoken(msg.sender, lpAmount, ethAmount);
+
+        _burnEtoken(msg.sender, lpAmount);
 
         emit Withdraw(msg.sender, ethAmount);
     }
 
     // method for the team to send ETH to the pool, and the pool to update ETH balance in EXA token
     receive() external payable onlyTeam nonReentrant {
+        _totalEthAmount += msg.value;
+
+        /* This is for avoid a division error when addEthBalance() is the
+         * first interaction with the contract and _ethPerUnit is equal zero */
+        if (_ethPerUnit == 0) {
+            _ethPerUnit += (_initialMintSupply / _totalEthAmount);
+            emit TeamAddedETH(msg.sender, msg.value);
+            return;
+        }
         _updateBalance();
 
         emit TeamAddedETH(msg.sender, msg.value);
@@ -136,37 +178,14 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
     /* eToken methods */
 
     // method for mint EXA tokens and execute update ethPerUnit and totalEthAmount
-    function _mintEToken(address _to, uint256 _ethAmount)
-        internal
-        checkAmount(_ethAmount)
-        nonReentrant
-    {
-        /* This is for avoid a division error when mint() is the
-         * first interaction with the contract and _ethPerUnit is equal zero */
-        if (_ethPerUnit == 0) {
-            uint256 totalEthAmount = address(this).balance;
-            _ethPerUnit = (_initialMintSupply / totalEthAmount);
-        }
-
-        uint256 _exaAmount;
-        _exaAmount = _ethAmount * _ethPerUnit;
+    function _mintEToken(address _to, uint256 _exaAmount) internal {
         _mint(_to, _exaAmount);
 
         _updateBalance();
     }
 
     // method for burn EXA tokens and update ethPerUnit and totalEthAmount
-    function _burnEtoken(
-        address _from,
-        uint256 exaAmount,
-        uint256 _ethAmount // is calculated on ETHPool so is cheaper insert it as parameter and not calculate again
-    ) internal checkAmount(_ethAmount) {
-        if (
-            super.totalSupply() < exaAmount ||
-            super.balanceOf(_from) < exaAmount
-        ) {
-            revert Error__NotEnoughAmount(exaAmount, super.balanceOf(_from));
-        }
+    function _burnEtoken(address _from, uint256 exaAmount) internal {
         _burn(_from, exaAmount);
 
         _updateBalance();
@@ -174,23 +193,26 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
 
     // method for update ethPerUnit
     function _updateBalance() internal {
-        uint256 totalSupply = super.totalSupply();
-        uint256 totalEthAmount = address(this).balance;
-
         // this is for avoid a division error
-        if (totalSupply == 0 && totalEthAmount == 0) {
+        if (_totalExaAmount == 0 && _totalEthAmount == 0) {
             _ethPerUnit = 0;
             return;
         }
+        console.log("------------");
+        console.log("_updateBalance");
+        console.log("_totalEthAmount: ", _totalEthAmount);
+        console.log("_totalExaAmount: ", _totalExaAmount);
 
         bool success;
-        (success, _ethPerUnit) = totalSupply.tryDiv(totalEthAmount);
+        (success, _ethPerUnit) = _totalExaAmount.tryDiv(_totalEthAmount);
         if (!success) {
-            revert Error__DivFailed(totalSupply, totalEthAmount);
+            revert Error__DivFailed(_totalExaAmount, _totalEthAmount);
         }
+        console.log("_ethPerUnit: ", _ethPerUnit);
+        console.log("------------");
     }
 
-    // function for updating the _team if is necessary
+    // function for updating the _team if necessary
     function updateTeam(address newTeam) external onlyTeam {
         _team = newTeam;
     }
@@ -201,17 +223,25 @@ contract ETHPool is ERC20, Ownable, ReentrancyGuard {
         return _ethPerUnit;
     }
 
-    function getInitialMintSupply() external pure returns (uint256) {
-        return _initialMintSupply;
-    }
-
     function getEthStablePrice() public view returns (uint256) {
         (, int256 ethStablePrice, , , ) = priceFeedV3Aggregator
             .latestRoundData();
         return uint256(ethStablePrice);
     }
 
+    function getTotalEthAmount() external view returns (uint256) {
+        return _totalEthAmount;
+    }
+
+    function getTotalExaAmount() external view returns (uint256) {
+        return _totalExaAmount;
+    }
+
     function getTeam() external view returns (address) {
         return _team;
+    }
+
+    function getInitialMintSupply() external pure returns (uint256) {
+        return _initialMintSupply;
     }
 }
