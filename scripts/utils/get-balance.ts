6b3ccffa4f9f61d7ff97cefd2b935f5fc8b617dd
NicoBarragan <nbarragan01@gmail.com>
2022-07-12 02:34:17 -0300

    refactor(get-balance.ts): updated function name in order to be called as equal as the file

--- scripts/utils/get-balance.ts
+++ scripts/utils/get-balance.ts
@@ -2,7 +2,7 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
 
-export default async function getContractBalance(
+export default async function getBalance(
   contractAddress: string
 ): Promise<BigNumber> {
   const balance = await ethers.provider.getBalance(contractAddress);
