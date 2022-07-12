41ae95815eec892792f8393c93fc5d6282f4e2d9
NicoBarragan <nbarragan01@gmail.com>
2022-07-12 02:38:37 -0300

    feat(int-test-eth-pool-test.ts): added 'connect' from contract to signers. implemented a timeout in the test for avoiding an error because of time execution. Exactly solidity challenge successfully tested on testnet

--- test/integration/int-test-eth-pool.test.ts
+++ test/integration/int-test-eth-pool.test.ts
@@ -1,18 +1,18 @@
 import deployEthPool from "../../scripts/eth-pool/deploy-eth-pool";
-import getContractBalance from "../../scripts/utils/get-balance";
-import { ethers } from "hardhat";
-import { ETHPool, ERC20 } from "../../typechain";
+import getBalance from "../../scripts/utils/get-balance";
+import { ETHPool } from "../../typechain";
 import { expect, use } from "chai";
 import "@nomiclabs/hardhat-ethers";
 import { waffleChai } from "@ethereum-waffle/chai";
-import { BigNumber, ContractTransaction, Signer } from "ethers";
+import { ethers } from "hardhat";
+import { BigNumber, ContractTransaction } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 const logger = require("pino")();
 use(waffleChai);
 
-const { DAI_ADDRESS } = process.env;
 const found = process.argv.indexOf("--network");
 const networkName = process.argv[found + 1]; // this for check after in etherscscan
+const TIMEOUT = 1000 * 60 * 2; // 2 minutes
 const GAS_LIMIT = 2074040;
 
 describe("integration-ETHPool", () => {
@@ -37,90 +37,93 @@ describe("integration-ETHPool", () => {
     ethPool = (await deployEthPool()) as ETHPool;
     logger.info(`ETHPool contract successfully deployed at ${ethPool.address}`);
 
+    ethPoolInitialBalance = await ethPool.balanceOf(userA.address);
     userAInitialBalance = await ethers.provider.getBalance(userA.address);
     teamInitialBalance = await ethers.provider.getBalance(team.address);
     userBInitialBalance = await ethers.provider.getBalance(userB.address);
-    ethPoolInitialBalance = await getContractBalance(ethPool.address);
-    zero = BigNumber.from("0");
+    zero = ethers.utils.parseEther("0");
   });
 
-  it("should complete the challenge proposed correctly", async function (done) {
-    this.timeout(20000); // for giving enough time for the tx to be completed
+  it("should complete the challenge proposed correctly", function (done) {
+    this.timeout(TIMEOUT);
 
-    try {
-      const userAEthAmount = userAInitialBalance.div(10);
-      logger.info(`supplying with ETH from the userA...`);
-      tx = await ethPool.supply({
-        from: userA.address,
-        value: userAEthAmount,
-        gasLimit: GAS_LIMIT,
-      });
-      await tx.wait();
-      logger.info(`supplied with ETH with userA!`);
+    const test = async () => {
+      try {
+        const userAEthAmount = userAInitialBalance.div(10);
+        logger.info(`userAInitialBalance:   ${userAInitialBalance}`);
+        logger.info(`userAEthAmount:        ${userAEthAmount}`);
+        logger.info(`supplying with ETH from the userA...`);
+        tx = await ethPool.supply({
+          from: userA.address,
+          value: userAEthAmount,
+          gasLimit: GAS_LIMIT,
+        });
+        await tx.wait();
+        logger.info(`supplied with ETH with userA!`);
 
-      const teamBalance = await ethers.provider.getBalance(team.address);
-      logger.info(`team balance: ${teamBalance}`);
-      const ethAmountTeam = teamBalance.div(10);
-      logger.info(`ethAmountTeam: ${ethAmountTeam}`);
-      logger.info(`Adding ETH from TEAM...`);
-      tx = await team.sendTransaction({
-        to: ethPool.address,
-        value: ethAmountTeam,
-        gasLimit: GAS_LIMIT,
-      });
-      await tx.wait();
-      logger.info(`ETH from TEAM added!`);
+        const teamBalance = teamInitialBalance;
+        const ethAmountTeam = teamBalance.div(10);
+        logger.info(`team balance:   ${teamBalance}`);
+        logger.info(`ethAmountTeam:  ${ethAmountTeam}`);
+        logger.info(`Adding ETH from TEAM...`);
+        tx = await team.sendTransaction({
+          to: ethPool.address,
+          value: ethAmountTeam,
+          gasLimit: GAS_LIMIT,
+        });
+        await tx.wait();
+        logger.info(`ETH from TEAM added!`);
 
-      const userBEthAmount = userBInitialBalance.div(10);
-      logger.info(`userBEthAmount: ${userBEthAmount}`);
-      logger.info(`supplying with ETH from the userB...`);
-      tx = await ethPool.supply({
-        from: userB.address,
-        value: userBEthAmount,
-        gasLimit: GAS_LIMIT,
-      });
-      await tx.wait();
-      logger.info(`supplied with ETH with userB!`);
+        const userBEthAmount = userBInitialBalance.div(10);
+        logger.info(`userBEthAmount: ${userBEthAmount}`);
+        logger.info(`supplying with ETH from the userB...`);
+        tx = await ethPool.connect(userB).supply({
+          from: userB.address,
+          value: userBEthAmount,
+          gasLimit: GAS_LIMIT,
+        });
+        await tx.wait();
+        logger.info(`supplied with ETH with userB!`);
 
-      const userAExaBalance = await ethPool.balanceOf(userA.address);
-      logger.info(`Withdrawing all from the userA...`);
-      const userAWithdrawTx = await ethPool.withdraw(userAExaBalance, {
-        from: userA.address,
-        gasLimit: GAS_LIMIT,
-      });
-      await tx.wait();
-      logger.info(`Successful withdrawal of userA!`);
+        const userAExaBalance = await ethPool.balanceOf(userA.address);
+        logger.info(`Withdrawing all from the userA...`);
+        const userAWithdrawTx = await ethPool.withdraw(userAExaBalance, {
+          from: userA.address,
+        });
+        await userAWithdrawTx.wait();
+        logger.info(`Successful withdrawal of userA!`);
 
-      const userBExaBalance = await ethPool.balanceOf(userB.address);
-      logger.info(`Withdrawing all from the userB...`);
-      const userBWithdrawTx = await ethPool.withdraw(userBExaBalance, {
-        from: userB.address,
-        gasLimit: GAS_LIMIT,
-      });
-      await tx.wait();
-      logger.info(`Successful withdrawal of userB!`);
+        const userBExaBalance = await ethPool.balanceOf(userB.address);
+        logger.info(`Withdrawing all from the userB...`);
+        const userBWithdrawTx = await ethPool
+          .connect(userB)
+          .withdraw(userBExaBalance, {
+            from: userB.address,
+          });
+        await userBWithdrawTx.wait();
+        logger.info(`Successful withdrawal of userB!`);
 
-      const ethPoolFinalBalance = await getContractBalance(ethPool.address);
-      logger.info(`The ETHPool ETH at the end is ${ethPoolFinalBalance}`);
-
-      logger.info(
-        `TX's successfully finished! 
+        logger.info(
+          `TX's successfully finished! 
         Here you can see the ETHPool contract tx's: https://${networkName}.etherscan.io/address/${ethPool.address}`
-      );
+        );
 
-      expect(ethPoolFinalBalance).to.be.equal(ethPoolInitialBalance);
-      expect(ethPoolFinalBalance).to.be.equal(zero);
-      expect(userAWithdrawTx).to.changeEtherBalance(
-        userA.address,
-        userAEthAmount.add(ethAmountTeam)
-      );
-      expect(userBWithdrawTx).to.changeEtherBalance(
-        userB.address,
-        userBEthAmount
-      );
-    } catch (err) {
-      logger.eror(err);
-    }
+        const ethPoolFinalBalance = await getBalance(ethPool.address);
+
+        expect(ethPoolFinalBalance).to.be.equal(ethPoolInitialBalance);
+        expect(ethPoolFinalBalance).to.be.equal(zero);
+        expect(userAWithdrawTx).to.changeEtherBalance(
+          userA.address,
+          userAEthAmount.add(ethAmountTeam)
+        );
+        expect(userBWithdrawTx).to.changeEtherBalance(
+          userB.address,
+          userBEthAmount
+        );
+      } catch (err) {
+        logger.error(err);
+      }
+    };
     done();
   });
 });
